f0b7262953128ad00858b60d5bae9d267242353e
Gene Ni <geneisjuan@gmail.com>
2025-02-09 22:32:12 +0000

    setup basic streamlit application with passkey authentication

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
-import streamlit as st
+# streamlit_app.py
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+from components.auth import check_password
+
+st.write("TOSAI")
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+st.write("Hello World!")
+st.button("Test button")
