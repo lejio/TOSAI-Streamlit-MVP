e99bb40f8b8754d03929c17681b69d857f3e2486
Gene Ni <geneisjuan@gmail.com>
2025-02-11 19:33:17 +0000

    prototype user input and output page for mvp v1

--- pages/mvp.py
+++ pages/mvp.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("TOSAI MVP")
+st.title("TOSAI MVP 1")
+
+input_col, pref_col = st.columns(2, gap='large')
+
+with input_col:
+    txt = st.text_area("Paste legal document", height=500)
+
+st.divider()
+
+with pref_col:
+    pref_tree = st.text_area("Edit your preference", height=500)
+
+st.write("## Output")
